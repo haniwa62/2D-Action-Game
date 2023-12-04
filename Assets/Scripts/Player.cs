42a3d6759bd7b770e31e7726041e78a22cf011f0
haniwa62 <221112@meiden-aitech.com>
2023-12-04 15:48:29 +0900

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float playerspeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 0, playerspeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, 0, (playerspeed * -1));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(playerspeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1 * playerspeed, 0, 0);
+        }
     }
 }
